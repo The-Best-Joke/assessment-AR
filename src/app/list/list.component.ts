934f9ef116395f700bd89f5ad2e7855f7e695c4b
Alejandro Salazar <alejandro.s@berkeley.edu>
2021-01-30 01:00:23 -0500

    Apply initial implementation of Challenges Service to ListComponent

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChallengesService } from '../services/challenges.service';
 
 @Component({
   selector: 'app-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  challenges$: Observable<any>;
 
-  constructor() { }
+  constructor(
+    private challengesService: ChallengesService
+    ) { }
 
   ngOnInit(): void {
+    this.challenges$ = this.challengesService.getChallenges();
   }
 
 }
